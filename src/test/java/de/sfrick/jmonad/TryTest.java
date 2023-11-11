ef8ae2f1dd6dbab826d9d437035f15b3dee1087c
sfrick <s.frick@reply.de>
2023-11-11 14:48:10 +0100

    feat: add try

---
+++ src/test/java/de/sfrick/jmonad/TryTest.java
@@ -0,0 +1,82 @@
+package de.sfrick.jmonad;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import de.sfrick.jmonad.Try.Failure;
+import de.sfrick.jmonad.Try.Success;
+
+public class TryTest {
+ 
+  @Test
+  void shouldReturnFailure() {
+    var ex = new RuntimeException("failure");
+    Try<String> actual = Try.failure(ex);
+
+    assertThat(actual).isInstanceOf(Failure.class);
+    assertThat(((Failure<String>)actual).cause()).isEqualTo(ex);
+  }
+
+  @Test
+  void shouldReturnSuccess() {
+    Try<String> actual = Try.success("success");
+
+    assertThat(actual).isInstanceOf(Success.class);
+    assertThat(((Success<String>)actual).value()).isEqualTo("success");
+  }
+
+  @Test
+  void shouldMapAPresentSuccessValue() {
+    Try<String> origin = Try.success("success");
+
+    Try<String> actual = origin.map(String::toUpperCase);
+
+    assertThat(actual.orElse("FAILURE")).isEqualTo("SUCCESS");
+  }
+
+  @Test
+  void shouldNotMapAFailureValue() {
+    var ex = new RuntimeException("failure");
+    Try<String> origin = Try.failure(ex);
+
+    Try<String> actual = origin.map(String::toUpperCase);
+
+    assertThat(actual.orElse("FAILURE")).isEqualTo("FAILURE");
+  }
+
+
+  Try<String> toSplit(String s) {
+    var ex = new RuntimeException("failure");
+    String[] split = s.split("&");
+    return split.length > 1 ? Try.success(split[1]) : Try.failure(ex);
+  }
+
+  @Test
+  void shouldFlatMapAPresentSuccessValue() {
+    Try<String> origin = Try.success("suc&cess");
+
+    Try<String> actual = origin.flatMap(this::toSplit);
+
+    assertThat(actual.orElse("FAILURE")).isEqualTo("cess");
+  }
+
+  @Test
+  void shouldNotFlatMapAFailureValue() {
+    var ex = new RuntimeException("failure");
+    Try<String> origin = Try.failure(ex);
+
+    Try<String> actual = origin.flatMap(this::toSplit);
+
+    assertThat(actual.orElse("FAILURE")).isEqualTo("FAILURE");
+  }
+
+  @Test
+  void shouldReturnOtherTry() {
+    var ex = new RuntimeException("fai&lure");
+    Try<String> origin = Try.failure(ex);
+    Try<String> anotherTry = Try.success("success");
+
+    assertThat(origin.or(anotherTry).orElse("default")).isEqualTo("success");
+  }
+}
